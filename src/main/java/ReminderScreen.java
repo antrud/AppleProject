c5db6939b0db1893873017e0b20bd7d68196c4ae
Anton Rudashko <anton.rudashko@welltech.com>
2023-02-04 16:16:45 +0200

    native ReminderTest

--- src/main/java/ReminderScreen.java
+++ src/main/java/ReminderScreen.java
@@ -1,2 +1,78 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class ReminderScreen {
+
+    AppiumDriver<MobileElement> driver;
+
+    @iOSXCUITFindBy(id = "Welcome to Reminders")
+    private MobileElement welcomeTitle;
+
+    @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeButton[`label == \"Continue\"`]")
+    private MobileElement btnContinue;
+
+    @iOSXCUITFindBy(accessibility = "Add List")
+    private MobileElement btnAddList;
+
+    @iOSXCUITFindBy(accessibility = "List Name")
+    private MobileElement etNewListName;
+
+    @iOSXCUITFindBy(accessibility = "Done")
+    private MobileElement btnDone;
+
+    @iOSXCUITFindBy(accessibility = "Empty list")
+    private MobileElement itEmptyList;
+
+    @iOSXCUITFindBy(accessibility = "Title")
+    private MobileElement etListItem;
+
+    @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeTextField[`label == \"Title\"`]")
+    private List<MobileElement> actualList;
+
+    public void acceptWelcomeScreen() {
+        try {
+            while(welcomeTitle.isDisplayed()) {
+                btnContinue.click();
+            }
+        } catch (Exception ex) {
+            System.out.println("Welcome screen element not found");
+        }
+    }
+
+    public void addNewListAndOpen(String listName) {
+        btnAddList.click();
+        etNewListName.sendKeys(listName);
+        btnDone.click();
+        driver.findElement(By.id(listName)).click();
+    }
+
+    public void addItemInList (List<String> textToFill) {
+        itEmptyList.click();
+
+        for (String item:
+             textToFill) {
+            etListItem.sendKeys(item + Keys.ENTER);
+        }
+
+        btnDone.click();
+    }
+
+    public void getItemsInList() {
+//        TODO: add all items.getValue() (except null) into List
+    }
+
+    public ReminderScreen(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(5)), this);
+    }
 }
--- src/test/java/ReminderTest.java
+++ src/test/java/ReminderTest.java
@@ -1,2 +1,90 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class ReminderTest {
+
+    private AppiumDriver<MobileElement> driver;
+    private DesiredCapabilities desiredCapabilities;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 14 Pro Max");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.2");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "com.apple.reminders");
+        desiredCapabilities.setCapability("appium:autoAcceptAlerts", true);
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AppiumDriver<MobileElement>(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void someTest() throws InterruptedException {
+        String listName = "My iOS Capabilities";
+
+        ReminderScreen screen = new ReminderScreen(driver);
+        screen.acceptWelcomeScreen();
+        screen.addNewListAndOpen(listName);
+
+        Map<String, Object> itemsInList = desiredCapabilities.asMap();
+        List<String> myList = convertMapToList(itemsInList);
+
+        screen.addItemInList(myList);
+
+        String listTitle = driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name=\"My iOS Capabilities\"]")).getText();
+        List<MobileElement> actualListItems = driver.findElements(By.xpath("(//XCUIElementTypeTextField[@name=\"Title\"])"));
+
+        Assert.assertEquals(listTitle, listName);
+        Assert.assertTrue(Objects.equals(myList, extractTextFromElements(actualListItems)));
+
+    }
+
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    public List<String> convertMapToList(Map<String, Object> map) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            list.add(entry.getKey() + "=" + entry.getValue());
+        }
+
+        return list;
+    }
+
+    public List<String> extractTextFromElements(List<MobileElement> listOfElements) {
+        List<String> textList = new ArrayList<>();
+        for (MobileElement item:
+             listOfElements) {
+            if(item.getText() != "Title") {
+                textList.add(item.getText());
+            }
+        }
+
+        return textList;
+    }
 }
